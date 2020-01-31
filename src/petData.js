d2b800f4464f92929c00453066c8c4e85b5a7700
Sophia Koeut <47515086+cantuus@users.noreply.github.com>
2020-01-31 08:57:21 -0800

    done with cat data

---
+++ src/petData.js
@@ -0,0 +1,53 @@
+const petData = {
+    cats: [
+        {
+            id: 1,
+            imageUrl: 'https://todaysveterinarynurse.com/wp-content/uploads/sites/3/2019/06/Adopt-a-Shelter-Cat.jpg',
+            name: 'Fluffy',
+            sex: 'Female',
+            age: 2,
+            breed: 'Mix',
+            story: `Owners couldn't take care of her anymore`
+        },
+        {
+            id: 2,
+            imageUrl: 'https://mk0nationaltodayijln.kinstacdn.com/wp-content/uploads/2019/04/national-siamese-cat-day-640x514.jpg',
+            name: 'Mochi',
+            sex: 'Male',
+            age: 3,
+            breed: 'Siamese',
+            story: `Found abandoned in a junkyard`
+        },
+        {
+            id: 3,
+            imageUrl: 'https://cdn2-www.cattime.com/assets/uploads/gallery/bengal-cats/bengal-5.jpg',
+            name: 'Tiger',
+            sex: 'Male',
+            age: 4,
+            breed: 'Bengal',
+            story: `Rescued from a neglectful home`
+        },
+        {
+            id: 4,
+            imageUrl: 'https://www.pethealthnetwork.com/sites/default/files/maine-coon-cat-484757920.jpg',
+            name: 'Mr. Stuffs',
+            sex: 'Male',
+            age: 3,
+            breed: 'Maine Coon',
+            story: `Found in the himalayans rescuing lost tigers`
+        },
+        {
+            id: 5,
+            imageUrl: 'https://cf.ltkcdn.net/cats/images/std/247865-699x450-sphynx-cat-rescue-shelters.jpg',
+            name: 'Raw',
+            sex: 'Male',
+            age: 3,
+            breed: 'Sphinx',
+            story: `Found in Egypt stuck on a pyramid`
+        },
+        
+
+
+
+    ]
+}
