dfe6fc25ab02c98731427158bea7a87cb60fcbd8
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2020-01-31 11:25:18 -0800

    cats q

---
+++ src/animals/animals-service.js
@@ -0,0 +1,12 @@
+/* eslint-disable quotes */
+const petData = require("../petData");
+const Queue = require("../Queue");
+
+const AnimalsService = {
+  getAllCats() {},
+  addCat() {},
+  getAllDogs() {},
+  addDog() {}
+};
+
+module.exports = AnimalsService;
--- src/petData.js
+++ src/petData.js
@@ -1,53 +1,119 @@
+/* eslint-disable quotes */
 const petData = {
-    cats: [
-        {
-            id: 1,
-            imageUrl: 'https://todaysveterinarynurse.com/wp-content/uploads/sites/3/2019/06/Adopt-a-Shelter-Cat.jpg',
-            name: 'Fluffy',
-            sex: 'Female',
-            age: 2,
-            breed: 'Mix',
-            story: `Owners couldn't take care of her anymore`
-        },
-        {
-            id: 2,
-            imageUrl: 'https://mk0nationaltodayijln.kinstacdn.com/wp-content/uploads/2019/04/national-siamese-cat-day-640x514.jpg',
-            name: 'Mochi',
-            sex: 'Male',
-            age: 3,
-            breed: 'Siamese',
-            story: `Found abandoned in a junkyard`
-        },
-        {
-            id: 3,
-            imageUrl: 'https://cdn2-www.cattime.com/assets/uploads/gallery/bengal-cats/bengal-5.jpg',
-            name: 'Tiger',
-            sex: 'Male',
-            age: 4,
-            breed: 'Bengal',
-            story: `Rescued from a neglectful home`
-        },
-        {
-            id: 4,
-            imageUrl: 'https://www.pethealthnetwork.com/sites/default/files/maine-coon-cat-484757920.jpg',
-            name: 'Mr. Stuffs',
-            sex: 'Male',
-            age: 3,
-            breed: 'Maine Coon',
-            story: `Found in the himalayans rescuing lost tigers`
-        },
-        {
-            id: 5,
-            imageUrl: 'https://cf.ltkcdn.net/cats/images/std/247865-699x450-sphynx-cat-rescue-shelters.jpg',
-            name: 'Raw',
-            sex: 'Male',
-            age: 3,
-            breed: 'Sphinx',
-            story: `Found in Egypt stuck on a pyramid`
-        },
-        
+  cats: [
+    {
+      id: 1,
+      imageUrl:
+        "https://todaysveterinarynurse.com/wp-content/uploads/sites/3/2019/06/Adopt-a-Shelter-Cat.jpg",
+      name: "Fluffy",
+      sex: "Female",
+      age: 2,
+      breed: "Mix",
+      story: `Owners couldn't take care of her anymore`,
+      date_admitted: 1 / 2 / 2020
+    },
+    {
+      id: 2,
+      imageUrl:
+        "https://mk0nationaltodayijln.kinstacdn.com/wp-content/uploads/2019/04/national-siamese-cat-day-640x514.jpg",
+      name: "Mochi",
+      sex: "Male",
+      age: 3,
+      breed: "Siamese",
+      story: `Found abandoned in a junkyard`,
+      date_admitted: 1 / 9 / 2020
+    },
+    {
+      id: 3,
+      imageUrl:
+        "https://cdn2-www.cattime.com/assets/uploads/gallery/bengal-cats/bengal-5.jpg",
+      name: "Tiger",
+      sex: "Male",
+      age: 4,
+      breed: "Bengal",
+      story: `Rescued from a neglectful home`,
+      date_admitted: 1 / 15 / 2020
+    },
+    {
+      id: 4,
+      imageUrl:
+        "https://www.pethealthnetwork.com/sites/default/files/maine-coon-cat-484757920.jpg",
+      name: "Mr. Stuffs",
+      sex: "Male",
+      age: 3,
+      breed: "Maine Coon",
+      story: `Found in the himalayans rescuing lost tigers`,
+      date_admitted: 1 / 22 / 2020
+    },
+    {
+      id: 5,
+      imageUrl:
+        "https://cf.ltkcdn.net/cats/images/std/247865-699x450-sphynx-cat-rescue-shelters.jpg",
+      name: "Raw",
+      sex: "Male",
+      age: 3,
+      breed: "Sphinx",
+      story: `Found in Egypt stuck on a pyramid`,
+      date_admitted: 1 / 29 / 2020
+    }
+  ],
+  dogs: [
+    {
+      id: 1,
+      imageUrl:
+        "https://cdn3-www.dogtime.com/assets/uploads/gallery/pekingese-dog-breed-pictures/6-floof.jpg",
+      name: "Hedda Mopper",
+      sex: "Female",
+      age: 2,
+      breed: "Pekingese",
+      story: `Owners couldn't take care of her anymore`,
+      date_admitted: 1 / 3 / 2020
+    },
+    {
+      id: 2,
+      imageUrl:
+        "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12213218/German-Shepherd-on-White-00.jpg",
+      name: "Radar",
+      sex: "Male",
+      age: 3,
+      breed: "German Shepherd",
+      story: `Owner passed away.`,
+      date_admitted: 1 / 10 / 2020
+    },
+    {
+      id: 3,
+      imageUrl:
+        "https://geniusvets.s3.amazonaws.com/gv-dog-breeds/american-pitbull-1.jpg",
+      name: "Lucky",
+      sex: "Female",
+      age: 2,
+      breed: "American Pitbull Terrier",
+      story: `Rescued from a neglectful home`,
+      date_admitted: 1 / 17 / 2020
+    },
+    {
+      id: 4,
+      imageUrl:
+        "https://vetstreet.brightspotcdn.com/dims4/default/514331d/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F26%2F0d%2F6443c92b4c67ae315a8bb8be16e1%2FChihuahua-AP-KIDP62-645lc061113.jpg",
+      name: "Pooch",
+      sex: "Male",
+      age: 3,
+      breed: "Chihuahua",
+      story: `Found under a bridge`,
+      date_admitted: 1 / 24 / 2020
+    },
+    {
+      id: 5,
+      imageUrl:
+        "https://www.dogbreedslist.info/uploads/allimg/dog-pictures/Boston-Terrier-1.jpg",
+      name: "Pingwin",
+      sex: "Male",
+      age: 3,
+      breed: "Terrier mix",
+      story: `Owner could not have dog in new apartment`,
+      date_admitted: 1 / 31 / 2020
+    }
+  ]
+};
 
-
-
-    ]
-}
+module.exports = petData;
